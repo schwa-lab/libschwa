13ff579c4d25629456135d7649c8793435e103a1
James Curran <james@it.usyd.edu.au>
2011-08-17 00:01:51 +1000

    * changed packages in setup.py so we get the schwa namespace * added .gitignore so we don't include compiled files

--- python/setup.py
+++ python/setup.py
@@ -1,19 +1,23 @@
+# -*- coding: utf-8 -*-
+
 from distutils.core import setup, Extension
 
-module1 = Extension('token',
-                    language = 'c++',
-                    define_macros = [('MAJOR_VERSION', '1'),
-                                     ('MINOR_VERSION', '0')],
-                    include_dirs = ['/usr/local/include'],
-                    libraries = ['boost_iostreams'],
-                    library_dirs = ['/usr/local/lib'],
-                    sources = ['pytoken.cc', '../tokenizer.cc'])
+token = Extension('schwa.token',
+                  language = 'c++',
+                  define_macros = [('MAJOR_VERSION', '1'),
+                                   ('MINOR_VERSION', '0')],
+                  include_dirs = ['/user/local/include'],
+                  libraries = ['boost_iostreams'],
+                  library_dirs = ['/usr/local/lib'],
+                  sources = ['pytoken.cc', '../tokenizer.cc'])
 
 setup(name = 'token',
       version = '1.0',
-      description = 'Schwa tokenizer',
+      description = 'ə-lab tokenizer',
       author = 'James R. Curran',
       author_email = 'james.r.curran@sydney.edu.au',
       url = 'http://schwa.org/tokenizer',
       long_description = '''package for tokenizing raw text in Python''',
-      ext_modules = [module1])
+      packages = ['schwa'],
+      ext_modules = [token],
+)
