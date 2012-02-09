f2e0d79d9bf1f1ffe9cbfc62557a4f1b6e487d89
tim.dawborn <tim.dawborn@gmail.com>
2012-02-09 11:49:30 +1100

    * renamed .hpp -> .h * initial support for streaming unserialization

--- include/schwa/hashtable.hpp
+++
@@ -1,4 +0,0 @@
-#include <schwa/hashtable/size.hpp>
-#include <schwa/hashtable/base.hpp>
-#include <schwa/hashtable/entry.hpp>
-#include <schwa/hashtable/ordered.hpp>
---
+++ src/schwa/docrep/wire_istream.cc
@@ -0,0 +1,24 @@
+#include <schwa/docrep.h>
+
+
+namespace schwa { namespace docrep {
+
+void
+WireIStream::_next(void) {
+  _has_next = _unpacker.next(&_result);
+  std::cerr << "WireIStream::_next _has_next=" << _has_next << std::endl;
+  if (!_has_next) {
+    const auto read = _in.readsome(_unpacker.buffer(), _unpacker.buffer_capacity());
+    std::cerr << "WireIStream::_next read=" << read << std::endl;
+    if (read == 0)
+      _eof = true;
+    else {
+      _unpacker.buffer_consumed(read);
+      _has_next = _unpacker.next(&_result);
+    }
+  }
+  std::cerr << "WireIStream::_next _has_next=" << _has_next << " _result=" << _result.get() << std::endl;
+}
+
+
+} }
