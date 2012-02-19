1e2c0a315fecb0f2a1d1df5dde9c5b09341a5a80
Tim Dawborn <tim.dawborn@gmail.com>
2012-02-20 08:19:54 +1100

    * more work on Python API

--- api/python/docrep/meta.py
+++ api/python/docrep/meta.py
@@ -12,10 +12,15 @@ class DocrepMetaclass(type):
         if v.name is None:
           v.name = k
 
+    ofields = list(fields)
+    ofields_inv = dict((v, i) for i, v in enumerate(ofields))
+
     for k in fields:
       attrs[k] = None
     klass = super(DocrepMetaclass, mklass).__new__(mklass, name, bases, attrs)
     klass._docrep_fields = fields
+    klass._docrep_ofields = ofields
+    klass._docrep_ofields_inv = ofields_inv
     return klass
 
 
@@ -33,9 +38,20 @@ class Annotation(object):
   __metaclass__ = AnnotationMetaclass
 
   def __init__(self, **kwargs):
+    self._docrep_set = [False] * len(self._docrep_fields)
     for k, v in kwargs.iteritems():
       if k in self._docrep_fields:
         setattr(self, k, v)
+
+  def __setattr__(self, key, val):
+    if key in self._docrep_fields:
+      idx = self._docrep_ofields_inv[key]
+      self._docrep_set[idx] = True
+    self.__dict__[key] = val
+
+  @classmethod
+  def _update_fields(klass, fields):
+    print '_update_ofields', klass, fields
 
 
 class Document(object):
