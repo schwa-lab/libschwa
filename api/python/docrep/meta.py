0cab78cc2b49c0b89791f304c2a9a49da0dfe04e
Tim Dawborn <tim.dawborn@gmail.com>
2012-02-21 15:31:43 +1100

    * initial working version of the docrep reading and writing API

--- api/python/docrep/meta.py
+++ api/python/docrep/meta.py
@@ -1,31 +1,49 @@
 from .fields import Annotations, Field
+from .utils import pluralise
 
 __all__ = ['Annotation', 'Document', 'Token']
 
 
 class DocrepMetaclass(type):
   def __new__(mklass, klass_name, bases, attrs, add_annotations):
-    annotations = {}
-    fields = {}
+    meta = attrs.get('Meta', None)
+
+    # construct the class
+    klass = super(DocrepMetaclass, mklass).__new__(mklass, klass_name, bases, attrs)
+    klass._docrep_name = klass_name.split('.')[-1]
+
+    # discover the Field and Annotations instances
+    annotations, fields, fields_s2py = {}, {}, {}
     for base in bases:
       if hasattr(base, '_docrep_annotations'):
         annotations.update(base._docrep_annotations)
       if hasattr(base, '_docrep_fields'):
         fields.update(base._docrep_fields)
+        fields_s2py.update(base._docrep_fields_s2py)
     for name, field in attrs.iteritems():
       if isinstance(field, Annotations):
         annotations[name] = field
       elif isinstance(field, Field):
         if field.name is None:
           field.name = name
-        fields[field.name] = field
+        fields[name] = field
+        fields_s2py[field.name] = name
 
-    klass = super(DocrepMetaclass, mklass).__new__(mklass, klass_name, bases, attrs)
-    klass._docrep_name = klass_name.split('.')[-1]
+    # adds the Field and Annotation information appropriately
     klass._docrep_fields = fields
+    klass._docrep_fields_s2py = fields_s2py
     if add_annotations:
       klass._docrep_annotations = annotations
-    klass._update_fields(())
+
+    # add the plural name
+    if hasattr(meta, 'plural'):
+      klass._docrep_plural = meta.plural
+    else:
+      klass._docrep_plural = pluralise(klass._docrep_name)
+
+    # ordered stream field attributes
+    klass._docrep_osfields = list(sorted(klass._docrep_fields_s2py))
+    klass._docrep_osfields_inv = dict((v, i) for i, v in enumerate(klass._docrep_osfields))
     return klass
 
 
@@ -41,7 +59,7 @@ class DocumentMetaclass(DocrepMetaclass):
 
 class Base(object):
   def __init__(self, **kwargs):
-    self._docrep_set = [False] * len(self._docrep_fields)
+    self._docrep_is_set = set()
     for name in self._docrep_fields:
       self.__dict__[name] = None
     for k, v in kwargs.iteritems():
@@ -50,19 +68,31 @@ class Base(object):
 
   def __setattr__(self, key, val):
     if key in self._docrep_fields:
-      idx = self._docrep_ofields_inv[key]
-      self._docrep_set[idx] = True
+      self._docrep_is_set.add(key)
     self.__dict__[key] = val
 
   @classmethod
-  def _update_fields(klass, fields):
-    old_fields = set(klass._docrep_fields)
-    new_fields = set(fields)
-    for field in new_fields - old_fields:
-      klass._docrep_fields[field] = Field(field)
+  def sidx_for_pyname(klass, py_name):
+    return klass._docrep_osfields_inv[klass._docrep_fields[py_name].name]
 
-    klass._docrep_ofields = list(klass._docrep_fields)
-    klass._docrep_ofields_inv = dict((v, i) for i, v in enumerate(klass._docrep_ofields))
+  @classmethod
+  def pyname_for_sidx(klass, sidx):
+    return klass._docrep_fields_s2py[klass._docrep_osfields[sidx]]
+
+  @classmethod
+  def update_osfields(klass, osfields):
+    osfields = list(osfields)
+    old_osfields = set(klass._docrep_osfields)
+    new_osfields = set(osfields)
+
+    for field in new_osfields - old_osfields:
+      klass._docrep_fields[field] = Field(field)
+      klass._docrep_fields_s2py[field] = field
+    for field in old_osfields - new_osfields:
+      osfields.append(field)
+
+    klass._docrep_osfields = osfields
+    klass._docrep_osfields_inv = dict((v, i) for i, v in enumerate(osfields))
 
 
 class Annotation(Base):
@@ -71,6 +101,7 @@ class Annotation(Base):
 
 class Document(Base):
   __metaclass__ = DocumentMetaclass
+
   def __init__(self, **kwargs):
     for name in self._docrep_annotations:
       self.__dict__[name] = []
