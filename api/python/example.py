9190a504aadf7e1f2785a106f41bee41a60cabba
tim.dawborn <tim.dawborn@gmail.com>
2012-03-12 15:07:16 +1100

    * corrected the discovery of already-existing classes in the reader

--- api/python/example.py
+++ api/python/example.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import docrep
 
-#class Sentence(docrep.Annotation):
+class Sentence(docrep.Annotation):
+  my_field = docrep.Range('Token', sname='span')
   #token_begin = docrep.Pointer('Token', sname='begin')
   #token_end   = docrep.Pointer('Token', sname='end')
 
@@ -10,9 +11,9 @@ import docrep
   #pos  = docrep.Field(sname='gold_pos')
 
 
-#class Document(docrep.Document):
-  #tokens    = docrep.Annotations('Token')
-  #sentences = docrep.Annotations('Sentence', sname='CoNLL03Sentence')
+class Document(docrep.Document):
+  tokens    = docrep.Annotations('Token')
+  sentences = docrep.Annotations('Sentence')
 
   #def token_range(self, start, finish):
     #for i in xrange(start._dr_index, finish._dr_index + 1):
@@ -20,9 +21,9 @@ import docrep
 
 
 with open('eng.testa.docrep', 'rb') as f:
-  reader = docrep.Reader()
+  reader = docrep.Reader(Document)
   for doc in reader.stream(f):
-    print doc.filename, len(doc.tokens), len(doc.sentences)
+    print type(doc), doc.filename, len(doc.tokens), len(doc.sentences)
     print [t.raw for t in doc.tokens[doc.sentences[0].span]]
     #for sent in doc.sentences:
       #print list(doc.token_range(sent.token_begin, sent.token_end))
