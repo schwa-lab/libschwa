53fa5c43e02c68ec76424325aa894294851d7747
Joel Nothman <joel.nothman@gmail.com>
2012-03-28 17:49:13 +1100

    Export some functions to testutil

--- api/python/tests/test_sanity.py
+++ api/python/tests/test_sanity.py
@@ -3,8 +3,9 @@ Tests ensuring that what is written out is identical to what is read in.
 """
 
 from unittest import TestCase
-from StringIO import StringIO
 from schwa import dr
+
+from testutil import write_x_read_y, write_read
 
 class DocWithoutFields(dr.Document):
     pass
@@ -18,19 +19,6 @@ class Annot(dr.Annotation):
 class DocWithAnnotsAndPointer(dr.Document):
     annots = dr.Annotations('Annot')
     special_annot = dr.Pointer('Annot')
-
-def write_x_read_y(doc, class2=None):
-    """typecast doc as class2"""
-    print 'Writing {0}'.format(doc.__class__.__name__)
-    f = StringIO()
-    dr.Writer(f).write_doc(doc)
-    f.seek(0)
-    print 'Reading {0}'.format(class2.__name__)
-    return dr.Reader(class2).stream(f).next()
-
-
-def write_read(doc):
-    return write_x_read_y(doc, doc.__class__)
 
 class SameModelTests(TestCase):
     
