f6924ccaaf438b15358b20d743c8959376447923
tim.dawborn <tim.dawborn@gmail.com>
2012-03-28 22:32:25 +1100

    * fixed whitespace

--- api/python/tests/test_fields.py
+++ api/python/tests/test_fields.py
@@ -1,34 +1,36 @@
-
 from unittest import TestCase
-from schwa import dr
 
+from schwa import dr
 from testutil import write_x_read_y, write_read
 
+
 class DocWithoutFields(dr.Document):
-    pass
+  pass
+
 
 class FieldWithDefault(dr.Field):
-    def default(self):
-        return 'default'
+  def default(self):
+    return 'default'
+
 
 class DocWithDefaultField(dr.Document):
-    field = FieldWithDefault()
+  field = FieldWithDefault()
+
 
 class FieldTests(TestCase):
-    def test_default_field(self):
-        doc = DocWithDefaultField()
-        self.assertEquals(doc.field, 'default')
+  def test_default_field(self):
+    doc = DocWithDefaultField()
+    self.assertEquals(doc.field, 'default')
 
-    def test_default_field_from_wire(self):
-        doc = DocWithoutFields()
-        doc = write_x_read_y(doc, DocWithDefaultField)
-        self.assertEquals(doc.field, 'default')
+  def test_default_field_from_wire(self):
+    doc = DocWithoutFields()
+    doc = write_x_read_y(doc, DocWithDefaultField)
+    self.assertEquals(doc.field, 'default')
 
-    def test_set_default_field_from_wire(self):
-        doc = DocWithoutFields()
-        doc = write_x_read_y(doc, DocWithDefaultField)
-        self.assertEquals(doc.field, 'default')
-        doc.field = 'value'
-        doc = write_read(doc)
-        self.assertEquals(doc.field, 'value')
-
+  def test_set_default_field_from_wire(self):
+    doc = DocWithoutFields()
+    doc = write_x_read_y(doc, DocWithDefaultField)
+    self.assertEquals(doc.field, 'default')
+    doc.field = 'value'
+    doc = write_read(doc)
+    self.assertEquals(doc.field, 'value')
--- api/python/tests/test_sanity.py
+++ api/python/tests/test_sanity.py
@@ -1,54 +1,57 @@
 """
 Tests ensuring that what is written out is identical to what is read in.
 """
-
 from unittest import TestCase
-from schwa import dr
 
+from schwa import dr
 from testutil import write_x_read_y, write_read
 
+
 class DocWithoutFields(dr.Document):
-    pass
+  pass
+
 
 class DocWithField(dr.Document):
-    field = dr.Field()
+  field = dr.Field()
+
 
 class Annot(dr.Annotation):
-    pass
+  pass
+
 
 class DocWithAnnotsAndPointer(dr.Document):
-    annots = dr.Annotations('Annot')
-    special_annot = dr.Pointer('Annot')
+  annots = dr.Annotations('Annot')
+  special_annot = dr.Pointer('Annot')
+
 
 class SameModelTests(TestCase):
-    
-    def test_pointer(self):
-        doc = DocWithAnnotsAndPointer()
-        doc.annots.create()
-        doc.special_annot = doc.annots[0]
-        doc = write_read(doc)
-        self.assertEquals(doc.special_annot, doc.annots[0])
+  def test_pointer(self):
+    doc = DocWithAnnotsAndPointer()
+    doc.annots.create()
+    doc.special_annot = doc.annots[0]
+    doc = write_read(doc)
+    self.assertEquals(doc.special_annot, doc.annots[0])
 
-    def test_null_pointer(self):
-        doc = DocWithAnnotsAndPointer()
-        doc.annots.create()
-        doc.special_annot = None
-        doc = write_read(doc)
-        self.assertEquals(doc.special_annot, None)
-        
+  def test_null_pointer(self):
+    doc = DocWithAnnotsAndPointer()
+    doc.annots.create()
+    doc.special_annot = None
+    doc = write_read(doc)
+    self.assertEquals(doc.special_annot, None)
+
+
 class DifferentModelTests(TestCase):
-    """Tests casting from one model to another via (de)serialisation"""
-    
-    def test_various(self):
-        doc = DocWithField()
-        doc = write_x_read_y(doc, DocWithField)
-        doc.field = 'foo'
-        doc = write_x_read_y(doc, DocWithoutFields)
-        doc = write_x_read_y(doc, DocWithAnnotsAndPointer)
-        doc.annots.create()
-        doc.special_annot = doc.annots[-1]
-        doc = write_x_read_y(doc, DocWithoutFields)
-        doc = write_x_read_y(doc, DocWithField)
-        self.assertEquals(doc.field, 'foo')
-        self.assertEquals(len(doc.annots), 1)
-        self.assertEquals(doc.special_annot, doc.annots[0])
+  """Tests casting from one model to another via (de)serialisation"""
+  def test_various(self):
+    doc = DocWithField()
+    doc = write_x_read_y(doc, DocWithField)
+    doc.field = 'foo'
+    doc = write_x_read_y(doc, DocWithoutFields)
+    doc = write_x_read_y(doc, DocWithAnnotsAndPointer)
+    doc.annots.create()
+    doc.special_annot = doc.annots[-1]
+    doc = write_x_read_y(doc, DocWithoutFields)
+    doc = write_x_read_y(doc, DocWithField)
+    self.assertEquals(doc.field, 'foo')
+    self.assertEquals(len(doc.annots), 1)
+    self.assertEquals(doc.special_annot, doc.annots[0])
