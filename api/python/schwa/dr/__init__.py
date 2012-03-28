0d8bd9f45781fcd6a91346d5b9ba4c42a398f9a0
Tim Dawborn <tim.dawborn@gmail.com>
2012-03-28 22:31:45 +1100

    * removed erronous import of missing file

--- api/python/schwa/dr/__init__.py
+++ api/python/schwa/dr/__init__.py
@@ -3,4 +3,3 @@ from .fields import Field, Pointer, Pointers, Range, Annotations, Singleton
 from .meta   import Annotation, Document, Token
 from .reader import Reader
 from .writer import Writer
-from .fieldtypes import DateTimeField
