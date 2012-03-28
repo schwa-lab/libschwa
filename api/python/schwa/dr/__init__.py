1774edc175800e333b42c1b27ba25b0e755c8b29
Joel Nothman <joel.nothman@gmail.com>
2012-03-28 17:53:59 +1100

    Test for default field values

--- api/python/schwa/dr/__init__.py
+++ api/python/schwa/dr/__init__.py
@@ -3,3 +3,4 @@ from .fields import Field, Pointer, Pointers, Range, Annotations, Singleton
 from .meta   import Annotation, Document, Token
 from .reader import Reader
 from .writer import Writer
+from .fieldtypes import DateTimeField
---
+++ api/python/tests/test_fields.py
@@ -0,0 +1,34 @@
+
+from unittest import TestCase
+from schwa import dr
+
+from testutil import write_x_read_y, write_read
+
+class DocWithoutFields(dr.Document):
+    pass
+
+class FieldWithDefault(dr.Field):
+    def default(self):
+        return 'default'
+
+class DocWithDefaultField(dr.Document):
+    field = FieldWithDefault()
+
+class FieldTests(TestCase):
+    def test_default_field(self):
+        doc = DocWithDefaultField()
+        self.assertEquals(doc.field, 'default')
+
+    def test_default_field_from_wire(self):
+        doc = DocWithoutFields()
+        doc = write_x_read_y(doc, DocWithDefaultField)
+        self.assertEquals(doc.field, 'default')
+
+    def test_set_default_field_from_wire(self):
+        doc = DocWithoutFields()
+        doc = write_x_read_y(doc, DocWithDefaultField)
+        self.assertEquals(doc.field, 'default')
+        doc.field = 'value'
+        doc = write_read(doc)
+        self.assertEquals(doc.field, 'value')
+
