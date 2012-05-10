4973e4c87be2ee805c326b82a471b80f1d6d564f
tim.dawborn <tim.dawborn@gmail.com>
2012-05-10 14:52:12 +1000

    * Added support for EncodedStringField, as per #1464

--- api/python/schwa/dr/__init__.py
+++ api/python/schwa/dr/__init__.py
@@ -2,10 +2,10 @@
 from .collections import StoreList
 from .exceptions import DependencyException, StoreException
 from .fields import Field, Pointer, Pointers, Singleton, Slice, Store
-from .field_types import DateTimeField
+from .field_types import DateTimeField, EncodedStringField
 from .meta   import Annotation, Document, Token
 from .reader import Reader
 from .writer import Writer
 
 
-__all__ = ['StoreList', 'DependencyException', 'StoreException', 'Field', 'Pointer', 'Pointers', 'Singleton', 'Slice', 'Store', 'DateTimeField', 'Annotation', 'Document', 'Token', 'Reader', 'Writer']
+__all__ = ['StoreList', 'DependencyException', 'StoreException', 'Field', 'Pointer', 'Pointers', 'Singleton', 'Slice', 'Store', 'DateTimeField', 'EncodedStringField', 'Annotation', 'Document', 'Token', 'Reader', 'Writer']
