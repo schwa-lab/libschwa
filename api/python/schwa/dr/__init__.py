e72ddf34a6d2208637abdd0323de8c5377288516
Joel Nothman <joel.nothman@gmail.com>
2012-05-14 20:45:10 +1000

    Add materialise_slices in decorators module

--- api/python/schwa/dr/__init__.py
+++ api/python/schwa/dr/__init__.py
@@ -8,5 +8,7 @@ from .meta   import Annotation, Document, Token
 from .reader import Reader
 from .writer import Writer
 
+from . import decorators
+
 
 __all__ = ['StoreList', 'Decorator', 'decorator', 'requires_decoration', 'method_requires_decoration', 'DependencyException', 'StoreException', 'Field', 'Pointer', 'Pointers', 'Singleton', 'Slice', 'Store', 'DateTimeField', 'EncodedStringField', 'Annotation', 'Document', 'Token', 'Reader', 'Writer']
