967feaab53df241015562fc6d7a4304a076086e9
Tim Dawborn <tim.dawborn@gmail.com>
2012-05-25 08:04:48 +1000

    * more work on C++ API

--- src/main/test.cc
+++ src/main/test.cc
@@ -2,8 +2,23 @@
 #include <schwa/base.h>
 #include <schwa/msgpack.h>
 #include <schwa/tokenizer.h>
+#include <schwa/dr/types.h>
+
+using namespace schwa;
+
+class Token : public dr::Annotation {
+public:
+  using dr::Annotation::Annotation;
+};
+
 
 int
 main(void) {
+  dr::Slice<uint64_t> slice;
+  dr::Slice<Token *> ptr_slice;
+  dr::Pointer<Token> ptr;
+  dr::Pointers<Token> ptrs;
+  dr::Store<Token> tokens;
+
   return 0;
 }
