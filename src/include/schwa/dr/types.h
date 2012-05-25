cba20686e8ad78c64a671e44324f67853b21d998
Tim Dawborn <tim.dawborn@gmail.com>
2012-05-25 11:57:19 +1000

    * added support for discovering the Python header files during "make depends" * more work on C++ docrep API

--- src/include/schwa/dr/types.h
+++ src/include/schwa/dr/types.h
@@ -1,10 +1,66 @@
 /* -*- Mode: C++; indent-tabs-mode: nil -*- */
-#include <boost/type_traits/is_base_of.hpp>
+#include <boost/typeof/typeof.hpp>
 
 namespace schwa {
   namespace dr {
 
+    typedef uint8_t loadmode_t;
+    static const loadmode_t LOAD_RW = 1;
+    static const loadmode_t LOAD_RO = 2;
+
+
+    class BaseDef {
+    protected:
+      const std::string _name;
+      const std::string _help;
+      const loadmode_t _mode;
+      std::string _serial;
+
+      BaseDef(Schema &schema, const std::string &name, const std::string &help, const loadmode_t mode, const std::string &serial) : _name(name), _help(help), _mode(mode), _serial(serial) {
+        schema.add(this);
+      }
+
+    public:
+      virtual ~BaseDef(void) { }
+    };
+
+
+    template <typename T, T fn>
+    class FieldDef;
+
+    template <typename R, typename T, R T::*member_obj_ptr>
+    class FieldDef<R T::*, member_obj_ptr> : public BaseDef {
+    public:
+      typedef R value_type;
+      typedef T class_type;
+
+      FieldDef(Schema &schema, const std::string &name, const std::string &help, const loadmode_t mode, const std::string &serial) : BaseDef(schema, name, help, mode, serial) { }
+      virtual ~FieldDef(void) { }
+    };
+
+
+    template <typename T, T fn>
+    class StoreDef;
+
+    template <typename R, typename T, R T::*member_obj_ptr>
+    class StoreDef<R T::*, member_obj_ptr> : public BaseDef {
+    public:
+      typedef R value_type;
+      typedef T class_type;
+
+      StoreDef(Schema &schema, const std::string &name, const std::string &help, const loadmode_t mode, const std::string &serial) : BaseDef(schema, name, help, mode, serial) { }
+      virtual ~StoreDef(void) { }
+    };
+
+
+    #define FIELD(member_obj_ptr) schwa::dr::FieldDef<BOOST_TYPEOF(member_obj_ptr), member_obj_ptr>
+    #define STORE(member_obj_ptr) schwa::dr::StoreDef<BOOST_TYPEOF(member_obj_ptr), member_obj_ptr>
+
+
     class Annotation {
+    public:
+      typedef TypeSchema Schema;
+
     protected:
       size_t _dr_index;
 
@@ -16,104 +72,13 @@ namespace schwa {
     };
 
 
-    template <typename T>
-    class Slice {
+    class Document {
     public:
-      T begin;
-      T end;
-
-      Slice(void) { }
-      Slice(const T &begin, const T &end) : begin(begin), end(end) { }
-    };
-
-
-    template <typename T>
-    class Pointer {
-    public:
-      static_assert(boost::is_base_of<Annotation, T>::value, "T must be a subclass of Annotation");
-      T *ptr;
-
-      Pointer(T *ptr=nullptr) : ptr(ptr) { }
-    };
-
-
-    template <typename T>
-    class Pointers {
-    public:
-      static_assert(boost::is_base_of<Annotation, T>::value, "T must be a subclass of Annotation");
-      std::vector<T *> items;
-    };
-
-
-    template <typename T>
-    class MemoryBlock {
-    private:
-      char *const _data;
-      const size_t _capacity;
-      size_t _size;
-
-    public:
-      MemoryBlock(const size_t capacity) : _data(new char[capacity * sizeof(T)]), _capacity(capacity), _size(0) {
-        assert(_data != nullptr);
-      }
-      ~MemoryBlock(void) {
-        delete [] _data;
-      }
-
-      inline size_t capacity(void) const { return _capacity; }
-      inline size_t size(void) const { return _size; }
-
-      inline char *
-      alloc(void) {
-        if (_size == _capacity)
-          return nullptr;
-        char *const ptr = &_data[_size * sizeof(T)];
-        ++_size;
-        return ptr;
-      }
-    };
-
-
-    template <typename T, size_t GROW_SIZE=32>
-    class Store {
-    public:
-      static_assert(boost::is_base_of<Annotation, T>::value, "T must be a subclass of Annotation");
-      static_assert(GROW_SIZE > 0, "GROW_SIZE must be positive");
+      typedef DocSchema Schema;
 
     protected:
-      std::vector<MemoryBlock<T> *> _blocks;
-      MemoryBlock<T> *_block;
-      size_t _size;
-
-    public:
-      Store(void) : _block(nullptr), _size(0) { }
-      Store(const Store &) = delete;
-      ~Store(void) {
-        delete _block;
-        for (auto &block : _blocks)
-          delete block;
-      }
-
-      inline size_t size(void) const { return _size; }
-      inline size_t nblocks(void) const { return (_block != nullptr) + _blocks.size(); }
-
-      T &
-      create(void) {
-        if (_block == nullptr)
-          _block = new MemoryBlock<T>(GROW_SIZE);
-
-        char *bytes = _block->alloc();
-        if (bytes == nullptr) {
-          _blocks.push_back(_block);
-          _block = new MemoryBlock<T>(GROW_SIZE);
-          bytes = _block->alloc();
-        }
-
-        T *const ptr = new (bytes) T();
-        ptr->set_dr_index(_size++);
-        return *ptr;
-      }
-
+      Document(void) = default;
+      Document(const Document &) = delete;
     };
 
   }
