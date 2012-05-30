145d93264ab4b4bef7c62867ba383884f2a13644
Tim Dawborn <tim.dawborn@gmail.com>
2012-05-31 08:40:23 +1000

    * more work on writing

--- src/lib/schwa/dr/writer.cc
+++ src/lib/schwa/dr/writer.cc
@@ -9,14 +9,14 @@ namespace mp = schwa::msgpack;
 namespace schwa { namespace dr {
 
 static void
-debug_schema(const BaseSchema &s, const bool is_doc_schema, const std::map<TypeInfo, size_t> &klass_map) {
+debug_schema(const BaseSchema &s, const bool is_doc_schema, const std::map<TypeInfo, size_t> &type_map) {
   std::cout << (is_doc_schema ? "__meta__" : s.serial) << " " << s.type << std::endl;
   for (auto &f : s.fields()) {
     std::cout << "  " << f->name << " " << f->is_pointer << " " << f->is_slice;
     if (f->is_pointer) {
       std::cout << " " << f->pointer_type().name;
-      const auto it = klass_map.find(f->pointer_type());
-      if (it == klass_map.end())
+      const auto it = type_map.find(f->pointer_type());
+      if (it == type_map.end())
         std::cout << " <unknown!>";
       else
         std::cout << " " << it->second;
@@ -26,40 +26,53 @@ debug_schema(const BaseSchema &s, const bool is_doc_schema, const std::map<TypeI
 }
 
 
-void
-Writer::write_klass_header(const BaseSchema &s, const bool is_doc_schema, const std::map<TypeInfo, size_t> &types) {
+static void
+write_klass_header(std::ostream &out, const BaseSchema &s, const bool is_doc_schema, const std::map<TypeInfo, size_t> &types) {
   // <klass> ::= ( <klass_name>, <fields> )
-  mp::write_array_header(_out, 2);
+  mp::write_array_header(out, 2);
 
   // <klass_name>
   const std::string name = is_doc_schema ? "__meta__" : s.serial;
-  mp::write_raw(_out, name.c_str(), name.size());
+  mp::write_raw(out, name.c_str(), name.size());
 
   // <fields> ::= [ <field> ]
-  mp::write_array_header(_out, s.fields().size());
+  mp::write_array_header(out, s.fields().size());
 
   for (auto &field : s.fields()) {
     // <field> ::= { <field_type> : <field_val> }
     const size_t nitems = 1 + field->is_pointer + field->is_slice;
-    mp::write_map_header(_out, nitems);
+    mp::write_map_header(out, nitems);
 
     // <field_type> ::= 0 # NAME => the name of the field
-    mp::write_uint_fixed(_out, 0);
-    mp::write_raw(_out, field->serial.c_str(), field->serial.size());
+    mp::write_uint_fixed(out, 0);
+    mp::write_raw(out, field->serial.c_str(), field->serial.size());
 
-    // <field_type> ::= 1 # POINTER_TO => the <klass_id> that this type points to
+    // <field_type> ::= 1 # POINTER_TO => the <type_id> that this type points to
     if (field->is_pointer) {
       const auto it = types.find(s.type);
       assert(it != types.end());
-      mp::write_uint_fixed(_out, 1);
-      mp::write_uint(_out, it->second);
+      mp::write_uint_fixed(out, 1);
+      mp::write_uint(out, it->second);
     }
 
     // <field_type> ::= 2 # IS_SLICE => whether or not this field is a "Slice" field
     if (field->is_slice) {
-      mp::write_uint_fixed(_out, 2);
-      mp::write_boolean(_out, true);
+      mp::write_uint_fixed(out, 2);
+      mp::write_boolean(out, true);
     }
+  }
+}
+
+
+template <typename T>
+static void
+write_instance(std::ostream &out, const T &obj, const BaseSchema &schema) {
+  size_t nfields = schema.fields().size(); // TODO this needs to be computed to account not writing out default fields
+  mp::write_map_header(out, nfields);
+  for (auto &field : schema.fields()) {
+    // TODO
+    (void)obj;
+    (void)field;
   }
 }
 
@@ -69,28 +82,28 @@ Writer::write(const Document &doc) {
   std::cout << "Writer::write(" << &doc << ")" << std::endl;
 
   // map each of the types to their unique klass id within the header
-  std::map<TypeInfo, size_t> klass_map;
-  size_t klass_id = 0;
-  klass_map[_dschema.type] = klass_id++;
+  std::map<TypeInfo, size_t> type_map;
+  size_t type_id = 0;
+  type_map[_dschema.type] = type_id++;
   for (auto &s : _dschema.schemas()) {
     const TypeInfo &type = s->type;
-    assert(klass_map.find(type) == klass_map.end());
-    klass_map[type] = klass_id++;
+    assert(type_map.find(type) == type_map.end());
+    type_map[type] = type_id++;
   }
 
   // <klasses> ::= [ <klass> ]
-  mp::write_array_header(_out, klass_map.size());
-  write_klass_header(_dschema, true, klass_map);
+  mp::write_array_header(_out, type_map.size());
+  write_klass_header(_out, _dschema, true, type_map);
   for (auto &s : _dschema.schemas())
-    write_klass_header(*s, false, klass_map);
+    write_klass_header(_out, *s, false, type_map);
 
   // <stores> ::= [ <store> ]
   mp::write_array_header(_out, _dschema.stores().size());
 
-  // <store> ::= ( <store_name>, <klass_id>, <store_nelem> )
+  // <store> ::= ( <store_name>, <type_id>, <store_nelem> )
   for (auto &store : _dschema.stores()) {
-    const auto it = klass_map.find(store->pointer_type());
-    assert(it != klass_map.end());
+    const auto it = type_map.find(store->pointer_type());
+    assert(it != type_map.end());
 
     mp::write_array_header(_out, 3);
     mp::write_raw(_out, store->serial.c_str(), store->serial.size());
@@ -99,12 +112,27 @@ Writer::write(const Document &doc) {
   }
 
   // <doc_instance> ::= <instances_nbytes> <instance>
-  // TODO
+  std::stringstream ss;
+  write_instance(ss, doc, _dschema);
+  mp::write_uint(_out, ss.tellp());
+  _out << ss.rdbuf();
 
-  std::cout << std::endl;
-  debug_schema(_dschema, true, klass_map);
+  // <instances_groups> ::= <instances_group>*
+  for (auto &store : _dschema.stores()) {
+    // reset the stringstream
+    ss.str("");
+    ss.clear();
+
+    const auto it = type_map.find(store->pointer_type());
+    (void)it;
+
+    // <instances_group> ::= <instances_nbytes> <instances>
+  }
+
+  std::cout << std::endl << std::endl;
+  debug_schema(_dschema, true, type_map);
   for (auto &s : _dschema.schemas())
-    debug_schema(*s, false, klass_map);
+    debug_schema(*s, false, type_map);
 
   // flush since we've finished writing a whole document
   _out.flush();
