7d6c4eb18ae4f1a11d892b9f82420a9358da0779
Tim Dawborn <tim.dawborn@gmail.com>
2012-06-06 09:22:23 +1000

    * vast improvements to test utils bytes diffing output

--- src/tests/unit_tests/test_utils.h
+++ src/tests/unit_tests/test_utils.h
@@ -1,20 +1,6 @@
 #include <boost/test/unit_test.hpp>
 
 
-template <typename OUT>
-static OUT &
-print_bytes(OUT &out, const uint8_t *bytes, const size_t size) {
-  out << "{";
-  for (size_t i = 0; i != size; ++i) {
-    if (i != 0)
-      out << ",";
-    out << "0x" << std::hex << static_cast<unsigned int>(bytes[i]);
-    out << " (" << bytes[i] << ")";
-  }
-  return out << "}";
-}
-
-
 boost::test_tools::predicate_result
 compare_bytes(const std::string &str, const uint8_t *expected, const size_t size);
 
