5e3faea8b7d5c78912cd02d97c982ef07b3a7776
tim.dawborn <tim.dawborn@gmail.com>
2012-06-06 13:20:47 +1000

    * renamed types.h to more sensible field_defs.h * removed old exceptions.h

--- src/include/schwa/dr.h
+++ src/include/schwa/dr.h
@@ -1,7 +1,6 @@
 /* -*- Mode: C++; indent-tabs-mode: nil -*- */
-#include <schwa/dr/exception.h>
 #include <schwa/dr/typeinfo.h>
 #include <schwa/dr/schema.h>
 #include <schwa/dr/fields.h>
-#include <schwa/dr/types.h>
+#include <schwa/dr/field_defs.h>
 #include <schwa/dr/writer.h>
