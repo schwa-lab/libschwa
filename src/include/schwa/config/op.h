f69893c50c4ccf722aeb0757be029549b5f41fb7
Tim Dawborn <tim.dawborn@gmail.com>
2012-06-26 17:28:02 +1000

    * added support for EnumOp

--- src/include/schwa/config/op.h
+++ src/include/schwa/config/op.h
@@ -29,8 +29,8 @@ namespace schwa {
       const T _default;
       T _value;
 
-      void
-      virtual _set(const std::string &value) {
+      virtual void
+      _set(const std::string &value) {
         std::stringstream ss(value);
         if (!(ss >> _value))
           throw ConfigException("Error setting value", _name, value);
@@ -60,6 +60,42 @@ namespace schwa {
 
 
     template <> inline void Op<std::string>::_set(const std::string &value) { _value = value; }
+
+
+    template <typename T>
+    class EnumOp : public Op<T> {
+    protected:
+      std::set<T> _options;
+
+      virtual void
+      _validate(void) {
+        if (_options.find(Op<T>::_value) == _options.end())
+          throw ConfigException("Invalid enum value", Op<T>::_name, Op<T>::_value);
+      }
+
+    public:
+      EnumOp(OpGroup &group, const std::string &name, const std::string &desc, std::initializer_list<T> options) : Op<T>(group, name, desc), _options(options) { }
+      EnumOp(OpGroup &group, const std::string &name, const std::string &desc, std::initializer_list<T> options, const T &default_) : Op<T>(group, name, desc, default_), _options(options) { }
+      virtual ~EnumOp(void) { }
+
+      virtual void
+      help(std::ostream &out, const std::string &prefix, unsigned int) const {
+        out << "  " << port::BOLD << prefix << Op<T>::_name << port::OFF << ": " << Op<T>::_desc;
+        out << " {";
+        bool first = true;
+        for (auto &it : _options) {
+          if (!first)
+            out << ",";
+          out << it;
+          first = false;
+        }
+        out << "}";
+        if (Op<T>::_has_default)
+          out << " (" << Op<T>::_default << ")";
+        out << std::endl;
+      }
+
+    };
 
   }
 }
