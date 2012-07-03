59e997f1a3d4f631155e8885fca9bea5c0d3244b
tim.dawborn <tim.dawborn@gmail.com>
2012-07-03 10:50:53 +1000

    * updated endianness converstion to use OS-specific APIs

--- src/lib/schwa/exception.cc
+++ src/lib/schwa/exception.cc
@@ -1,10 +1,8 @@
 /* -*- Mode: C++; indent-tabs-mode: nil -*- */
 #include <schwa/base.h>
-#include <schwa/port.h>
 
 
 namespace schwa {
-
 
 print_exception::print_exception(const Exception &e) : name(port::demangle_typeid(typeid(e).name())), e(e) { }
 print_exception::print_exception(const std::string &name, const Exception &e) : name(name), e(e) { }
@@ -13,6 +11,5 @@ std::ostream &
 print_exception::dump(std::ostream &out) const {
   return out << port::BOLD << '[' << name << "] " << port::RED << e.what() << port::OFF;
 }
-
 
 }
