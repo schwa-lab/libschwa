87b4a9f3b1dbbf9fdca8dce3d7ad3a18206f2a10
tim.dawborn <tim.dawborn@gmail.com>
2012-07-08 10:31:38 +1000

    * updated WriteBuffer experiments to subclass std::streambuf, as expected by the STL

--- src/include/schwa/io/buffer.h
+++ src/include/schwa/io/buffer.h
@@ -3,17 +3,19 @@
 namespace schwa {
   namespace io {
 
-    class WriteBuffer {
+    class WriteBuffer : public std::streambuf {
     public:
       static constexpr size_t DEFAULT_BLOCK_SIZE = 64;
       static constexpr size_t DEFAULT_POOL_SIZE = 4096;
       static constexpr size_t DEFAULT_NBLOCKS = 8;
+      static constexpr size_t DEFAULT_NPOOLS = 8;
 
-      struct BlockInfo {
+      struct Block {
         const size_t capacity;
+        char *const data;
         size_t size;
 
-        BlockInfo(size_t capacity, size_t size=0) : capacity(capacity), size(size) { }
+        Block(size_t capacity, char *data, size_t size=0) : capacity(capacity), data(data), size(size) { }
       };
 
     private:
@@ -24,26 +26,17 @@ namespace schwa {
       size_t _pool_used;
 
       const size_t _block_size;
-      char **_blocks;
-      BlockInfo *_block_info;
+      Block *_blocks;
       size_t _current_block;
       size_t _nblocks;
-
-      size_t _size;
-
-      size_t _lb_capacity;
-      size_t _lb_size;
-      char * _lb_upto;
 
       inline void
       next_block(void) {
         ++_current_block;
         if (_current_block == _nblocks) {
           _nblocks *= 2;
-          _blocks = static_cast<char **>(std::realloc(_blocks, _nblocks * sizeof(char *)));
+          _blocks = static_cast<Block *>(std::realloc(_blocks, _nblocks * sizeof(Block)));
           assert(_blocks != nullptr);
-          _block_info = static_cast<BlockInfo *>(std::realloc(_block_info, _nblocks * sizeof(BlockInfo)));
-          assert(_block_info != nullptr);
         }
       }
 
@@ -57,87 +50,99 @@ namespace schwa {
         }
       }
 
+    protected:
+      virtual int
+      overflow(const int c=EOF) override {
+        std::cout << "WriteBuffer::overflow(0x" << std::hex << c << std::dec << ")" << std::endl;
+        if (c == EOF)
+          return c;
+
+        // create a new block
+        _blocks[_current_block].size = pptr() - pbase();
+        next_block();
+
+        // move to the next pool if the current pool is full
+        if (_pool_used == _pool_size) {
+          next_pool();
+
+          char *p = static_cast<char *>(std::malloc(_pool_size));
+          assert(p != nullptr);
+          _pools[_current_pool] = p;
+          _pool_used = 0;
+        }
+
+        // initialise the new block with memory from a pool
+        char *const write_buf = &(_pools[_current_pool][_pool_used]);
+        new (_blocks + _current_block) Block(_block_size, write_buf);
+        _pool_used += _block_size;
+
+        // tell std::streambuf where the new write buffer is and write the overflow character
+        setp(write_buf, write_buf + _block_size);
+        *write_buf = static_cast<char>(c);
+        pbump(1);
+
+        return c;
+      }
+
+      virtual std::streamsize
+      xsputn(const char *const data, const std::streamsize nbytes) override {
+        std::cout << "WriteBuffer::xsputn(" << nbytes << ")" << std::endl;
+        // create a new block
+        _blocks[_current_block].size = pptr() - pbase();
+        next_block();
+
+        // initialise the new block with the provided data
+        char *const write_buf = const_cast<char *>(data);
+        new (_blocks + _current_block) Block(nbytes, write_buf, nbytes);
+
+        // tell std::streambuf where the new write buffer is
+        setp(write_buf, write_buf + nbytes);
+        pbump(nbytes);
+
+        return nbytes;
+      }
+
+
     public:
-      explicit WriteBuffer(size_t block_size=DEFAULT_BLOCK_SIZE, size_t nblocks=DEFAULT_NBLOCKS, size_t pool_size=DEFAULT_POOL_SIZE) :
+      explicit WriteBuffer(size_t block_size=DEFAULT_BLOCK_SIZE, size_t nblocks=DEFAULT_NBLOCKS, size_t pool_size=DEFAULT_POOL_SIZE, size_t npools=DEFAULT_NPOOLS) :
+          std::streambuf(),
           _pool_size(pool_size),
-          _pools(static_cast<char **>(std::malloc(nblocks * sizeof(char *)))),
-          _npools(nblocks),
+          _pools(static_cast<char **>(std::malloc(npools * sizeof(char *)))),
+          _npools(npools),
           _current_pool(0),
           _pool_used(0),
           _block_size(block_size),
-          _blocks(static_cast<char **>(std::malloc(nblocks * sizeof(char *)))),
-          _block_info(static_cast<BlockInfo *>(std::malloc(nblocks * sizeof(BlockInfo)))),
+          _blocks(static_cast<Block *>(std::malloc(nblocks * sizeof(Block)))),
           _current_block(0),
-          _nblocks(nblocks),
-          _size(0) {
+          _nblocks(nblocks) {
         assert(_pools != nullptr);
         assert(_blocks != nullptr);
-        assert(_block_info != nullptr);
 
         char *p = static_cast<char *>(std::malloc(pool_size));
         assert(p != nullptr);
         _pools[_current_pool] = p;
 
-        _blocks[_current_block] = &(_pools[_current_pool][_pool_used]);
+        char *const write_buf = &(_pools[_current_pool][_pool_used]);
+        new (_blocks + _current_block) Block(block_size, write_buf);
         _pool_used += _block_size;
-        new (_block_info + _current_block) BlockInfo(block_size);
-        _lb_capacity = block_size;
-        _lb_size = 0;
-        _lb_upto = _blocks[_current_block];
+
+        // tell std::streambuf where the write buffer is
+        setp(write_buf, write_buf + _block_size);
       }
 
-      ~WriteBuffer(void) {
+      virtual ~WriteBuffer(void) {
         for (size_t i = 0; i <= _current_pool; ++i)
           std::free(_pools[i]);
         std::free(_pools);
         std::free(_blocks);
-        std::free(_block_info);
       }
 
-      inline size_t size(void) const { return _size; }
-
-      void
-      put(const char c) {
-        if (_lb_size == _lb_capacity) {
-          _block_info[_current_block].size = _lb_size;
-          next_block();
-
-          if (_pool_used == _pool_size) {
-            next_pool();
-
-            char *p = static_cast<char *>(std::malloc(_pool_size));
-            assert(p != nullptr);
-            _pools[_current_pool] = p;
-            _pool_used = 0;
-          }
-
-          _blocks[_current_block] = &(_pools[_current_pool][_pool_used]);
-          _pool_used += _block_size;
-          new (_block_info + _current_block) BlockInfo(_block_size);
-
-          _lb_capacity = _block_size;
-          _lb_size = 0;
-          _lb_upto = _blocks[_current_block];
-        }
-
-        *_lb_upto = c;
-        _lb_upto += 1;
-        _lb_size += 1;
-        _size += 1;
-      }
-
-      void
-      write(const char *const data, const size_t nbytes) {
-        _block_info[_current_block].size = _lb_size;
-        next_block();
-
-        _blocks[_current_block] = const_cast<char *>(data);
-        new (_block_info + _current_block) BlockInfo(nbytes, nbytes);
-
-        _lb_capacity = nbytes;
-        _lb_size = nbytes;
-        _lb_upto = const_cast<char *>(data);
-        _size += nbytes;
+      size_t
+      size(void) const {
+        size_t size = 0;
+        for (size_t i = 0; i <= _current_block; ++i)
+          size += _blocks[i].size;
+        return size;
       }
     };
 
