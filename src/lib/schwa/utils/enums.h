42f44bdf3c9ac95f3640f7bfc86a9a50496ff991
Tim Dawborn <tim.dawborn@gmail.com>
2014-02-21 15:22:20 +1100

    Added additional from_underlying fixed type enum helper to go with to_underlying.

--- src/lib/schwa/utils/enums.h
+++ src/lib/schwa/utils/enums.h
@@ -7,6 +7,13 @@
 namespace schwa {
 
   template <typename E>
+  inline constexpr E
+  from_underlying(const typename std::underlying_type<E>::type e) {
+    static_assert(std::is_enum<E>::value, "must be used with an enum");
+    return static_cast<E>(e);
+  }
+
+  template <typename E>
   inline constexpr typename std::underlying_type<E>::type
   to_underlying(E e) {
     static_assert(std::is_enum<E>::value, "must be used with an enum");
