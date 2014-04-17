469a608dbdbbe5325feea8fdcf490d2388f64c72
Tim Dawborn <tim.dawborn@gmail.com>
2014-04-17 16:19:21 +1000

    Removed accidental inclusion of _impl header files.

--- src/lib/schwa/learn.h
+++ src/lib/schwa/learn.h
@@ -4,9 +4,7 @@
 
 #include <schwa/learn/enums.h>
 #include <schwa/learn/extract.h>
-#include <schwa/learn/extract_impl.h>
 #include <schwa/learn/feature_transformers.h>
 #include <schwa/learn/features.h>
-#include <schwa/learn/features_impl.h>
 
 #endif  // SCHWA_LEARN_H_
--- src/lib/schwa/learn/features_impl.h
+++ src/lib/schwa/learn/features_impl.h
@@ -27,7 +27,6 @@ namespace schwa {
       }
     }
 
-
   }
 }
 
