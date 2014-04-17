baf94d24cc44d8127567cb1051b878dc6eef8155
Tim Dawborn <tim.dawborn@gmail.com>
2014-04-17 16:17:06 +1000

    Added pkg-config brew dependency.

--- dist/libschwa.rb
+++ dist/libschwa.rb
@@ -5,6 +5,7 @@ class Libschwa < Formula
   url "https://github.com/schwa-lab/libschwa/releases/download/0.2.2/libschwa-0.2.2.tar.gz"
   sha1 "41d3e1a2dd3686da6ee0acb302e88eb84ca937bb"
 
+  depends_on "pkg-config"
   depends_on "zeromq" => :optional
 
   def install
