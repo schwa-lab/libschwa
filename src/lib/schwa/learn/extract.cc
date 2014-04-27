d45ab93f153b59ba2a60d19907131f4ae0d5c414
Tim Dawborn <tim.dawborn@gmail.com>
2014-04-27 18:53:21 +1000

    API tweak.

--- src/lib/schwa/learn/extract.cc
+++ src/lib/schwa/learn/extract.cc
@@ -13,15 +13,15 @@ namespace schwa {
 namespace learn {
 
 
-template <typename ForwardIterator>
+template <typename T>
 static std::string
-_word_form(ForwardIterator begin, ForwardIterator end) {
+_word_form(const T &string) {
   std::string form;
   form.reserve(6);
   u::CategoryName prev_category = u::CategoryName::Cn;
 
-  for ( ; begin != end; ++begin) {
-    const auto category = u::get_category(*begin);
+  for (const auto code_point : string) {
+    const auto category = u::get_category(code_point);
     if (category != prev_category)
       form.append(u::get_category_name(category));
     prev_category = category;
@@ -32,14 +32,13 @@ _word_form(ForwardIterator begin, ForwardIterator end) {
 
 std::string
 word_form(const UnicodeString &s) {
-  return _word_form(s.begin(), s.end());
+  return _word_form(s);
 }
 
 
 std::string
 word_form(const std::string &utf8) {
-  UTF8Decoder d(utf8);
-  return _word_form(d.begin(), d.end());
+  return _word_form(UTF8Decoder(utf8));
 }
 
 
