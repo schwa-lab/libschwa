feade259898719d79ab76845535df1db1ae71036
Tim Dawborn <tim.dawborn@gmail.com>
2014-06-16 21:28:15 +1000

    Updated SHA1 hash.

--- dist/libschwa.rb
+++ dist/libschwa.rb
@@ -3,7 +3,7 @@ require "formula"
 class Libschwa < Formula
   homepage "https://github.com/schwa-lab/libschwa"
   url "https://github.com/schwa-lab/libschwa/releases/download/0.3.0/libschwa-0.3.0.tar.gz"
-  sha1 "4d03883d2318352856c0a2dd6bc2bbf4bab6c202"
+  sha1 "9b44fb99547b3e8dd1a8e1e7bf24615f3f437bd6"
 
   depends_on "pkg-config"
   depends_on "zeromq" => :optional
