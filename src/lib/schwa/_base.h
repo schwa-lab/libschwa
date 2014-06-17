83105c37f648967fb74a11f8f02a5b2b808bd72f
Tim Dawborn <tim.dawborn@gmail.com>
2014-06-17 20:39:10 +1000

    Don't distribute _config.h (whoops).  Added `SCHWA_LIKELY` and `SCHWA_UNLIKELY` macro definitions to wrap around `__builtin_expect`.

---
+++ src/lib/schwa/_base.h
@@ -0,0 +1,15 @@
+/* -*- Mode: C++; indent-tabs-mode: nil -*- */
+#ifndef SCHWA__BASE_H_
+#define SCHWA__BASE_H_
+
+#ifdef HAVE_CONFIG_H
+  #include "_config.h"
+#endif
+
+#include <cstddef>
+#include <cstdint>
+
+#include <schwa/enums.h>
+#include <schwa/macros.h>
+
+#endif  // SCHWA__BASE_H_
