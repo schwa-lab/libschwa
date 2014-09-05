ec5cb259c19032b2883798c923ad45642401c0d4
Tim Dawborn <tim.dawborn@gmail.com>
2014-09-05 11:43:45 +1000

    Bumped homebrew recipe version.

--- dist/libschwa.rb
+++ dist/libschwa.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Libschwa < Formula
   homepage "https://github.com/schwa-lab/libschwa"
-  url "https://github.com/schwa-lab/libschwa/releases/download/0.3.0/libschwa-0.3.0.tar.gz"
-  sha1 "9b44fb99547b3e8dd1a8e1e7bf24615f3f437bd6"
+  url "https://github.com/schwa-lab/libschwa/releases/download/0.4.0/libschwa-0.4.0.tar.gz"
+  sha1 "b8d4f7796d55c5b2a733506e59382141fb0187f1"
 
   depends_on "pkg-config"
   depends_on "zeromq" => :optional
