3c3e8ac223872209df575d4b04cdfac2e9476d56
Tim Dawborn <tim.dawborn@gmail.com>
2014-10-02 12:52:57 +1000

    Changed schwa::Pool to provide block alignment guarentees. Useful if you need alignment requirements, such as when using SSE or AXV instructions.

--- src/lib/schwa/pool.h
+++ src/lib/schwa/pool.h
@@ -8,92 +8,61 @@
 
 #include <schwa/_base.h>
 
+
 namespace schwa {
 
   /**
-   * Objects allocated within a Pool WILL NOT have their destructors called when
-   * the Pool is destroyed. As such, it is only safe to allocate within the Pool
-   * objects that don't need destructors.
+   * Objects allocated within a Pool WILL NOT have their destructors called when the Pool is
+   * destroyed. As such, it is only safe to allocate within the Pool objects that don't need
+   * destructors.
+   *
+   * The starting address of blocks will be aligned to the \p alignment argument passed in to the
+   * constructor. If an alloc() request is larger than the block size, a single once-off block
+   * will be allocd large enough for the alloc() request.
    **/
   class Pool {
-  private:
-    class Block {
-    private:
-      const size_t _size;
-      size_t _upto;
-
-      explicit Block(size_t size) : _size(size), _upto(0) { }
-
-    public:
-      inline char *data(void) { return reinterpret_cast<char *>(this + 1); }
-      inline size_t size(void) const { return _size; }
-      inline size_t upto(void) const { return _upto; }
-
-      inline void *
-      alloc(size_t size) {
-        size_t upto = _upto + size;
-        if (upto > _size)
-          return nullptr;
-        void *ptr = data() + _upto;
-        _upto = upto;
-        return ptr;
-      }
-
-      inline void operator delete(void *ptr) {
-        std::free(ptr);
-      }
-
-      static Block *
-      create(size_t size) {
-        void *ptr = std::malloc(sizeof(Block) + size);
-        if (ptr == nullptr)
-          throw std::bad_alloc();
-        return new (ptr) Block(size);
-      }
-
-    private:
-      SCHWA_DISALLOW_COPY_AND_ASSIGN(Block);
-    };
-
-    const size_t _block_size;
-    Block *_current;
-    std::vector<Block *> _blocks;
-
   public:
-    explicit Pool(size_t block_size) : _block_size(block_size), _current(Block::create(block_size)) {
-      _blocks.push_back(_current);
+    static constexpr const size_t DEFAULT_ALIGNMENT = 32;  //!< The default memory alignment used for blocks.
+
+  private:
+    const size_t _block_size;  //!< The block size to alloc.
+    const size_t _alignment; //!< The starting address of all allocd blocks will have this alignment.
+    size_t _allocd;  //!< Number of bytes allocd in the current block.
+    size_t _used;  //!< Number of bytes used in the current block.
+    std::vector<void *> _blocks;  //<! The allocd blocks. The active block is the back-most element.
+
+    inline void
+    _alloc_new_block(const size_t nbytes) {
+      void *ptr;
+      const int ret = ::posix_memalign(&ptr, _alignment, nbytes);
+      if (SCHWA_UNLIKELY(ret != 0))
+        throw std::bad_alloc();
+      _allocd = nbytes;
+      _used = 0;
+      _blocks.push_back(ptr);
     }
 
+  public:
+    explicit Pool(size_t block_size, size_t alignment=DEFAULT_ALIGNMENT) : _block_size(block_size), _alignment(alignment) {
+      _alloc_new_block(_block_size);
+    }
     ~Pool(void) {
-      for (Block *b : _blocks)
-        delete b;
+      for (void *ptr : _blocks)
+        std::free(ptr);
     }
 
     template <typename T=void *>
     inline T
-    alloc(size_t size) {
-      void *ptr = _current->alloc(size);
-      if (ptr == nullptr) {
-        _current = Block::create(size > _block_size ? size : _block_size);
-        _blocks.push_back(_current);
-        ptr = _current->alloc(size);
-      }
+    alloc(const size_t nbytes) {
+      // Do we have room in the current block for the allocation request?
+      if (_used + nbytes > _allocd)
+        _alloc_new_block(nbytes > _block_size ? nbytes : _block_size);
+      void *const ptr = reinterpret_cast<uint8_t *>(_blocks.back()) + _used;
+      _used += nbytes;
       return static_cast<T>(ptr);
     }
 
     inline size_t nblocks(void) const { return _blocks.size(); }
-    inline size_t allocd(void) const {
-      size_t count = 0;
-      for (Block *b : _blocks)
-        count += b->size();
-      return count;
-    }
-    inline size_t used(void) const {
-      size_t count = 0;
-      for (Block *b : _blocks)
-        count += b->upto();
-      return count;
-    }
 
   private:
     SCHWA_DISALLOW_COPY_AND_ASSIGN(Pool);
@@ -108,10 +77,10 @@ namespace schwa {
   struct PoolAllocator {
     using const_pointer = const T *;
     using const_reference = const T &;
-    using difference_type = std::ptrdiff_t;
+    using difference_type = ptrdiff_t;
     using pointer = T *;
     using reference = T &;
-    using size_type = std::size_t;
+    using size_type = size_t;
     using value_type = T;
     template <class U> struct rebind { using other = PoolAllocator<U>; };
 
@@ -121,7 +90,7 @@ namespace schwa {
     template <typename U> PoolAllocator(const PoolAllocator<U> &o) : pool(o.pool) { }
 
     inline T *
-    allocate(const std::size_t n) {
+    allocate(const size_t n) {
       return pool.alloc<T *>(n * sizeof(T));
     }
 
@@ -132,7 +101,7 @@ namespace schwa {
     }
 
     inline void
-    deallocate(T *, std::size_t) { }
+    deallocate(T *, size_t) { }
 
     template <class U>
     inline void
@@ -148,7 +117,7 @@ namespace schwa {
  * std::new operator for use with a Pool.
  **/
 inline void *
-operator new(std::size_t nbytes, ::schwa::Pool &pool) {
+operator new(size_t nbytes, ::schwa::Pool &pool) {
   return pool.alloc(nbytes);
 }
 
--- src/lib/schwa/pool_test.cc
+++ src/lib/schwa/pool_test.cc
@@ -11,38 +11,39 @@ SUITE(schwa__pool) {
 TEST(test) {
   Pool p(64);
   CHECK_EQUAL(1, p.nblocks());
-  CHECK_EQUAL(64, p.allocd());
-  CHECK_EQUAL(0, p.used());
+  //CHECK_EQUAL(64, p.allocd());
+  //CHECK_EQUAL(0, p.used());
 
-  p.alloc(60);
+  void *ptr = p.alloc(60);
   CHECK_EQUAL(1, p.nblocks());
-  CHECK_EQUAL(64, p.allocd());
-  CHECK_EQUAL(60, p.used());
+  //CHECK_EQUAL(64, p.allocd());
+  //CHECK_EQUAL(60, p.used());
+  CHECK(reinterpret_cast<size_t>(ptr) % Pool::DEFAULT_ALIGNMENT == 0);
 
   p.alloc(4);
   CHECK_EQUAL(1, p.nblocks());
-  CHECK_EQUAL(64, p.allocd());
-  CHECK_EQUAL(64, p.used());
+  //CHECK_EQUAL(64, p.allocd());
+  //CHECK_EQUAL(64, p.used());
 
   p.alloc(80);
   CHECK_EQUAL(2, p.nblocks());
-  CHECK_EQUAL(144, p.allocd());
-  CHECK_EQUAL(144, p.used());
+  //CHECK_EQUAL(144, p.allocd());
+  //CHECK_EQUAL(144, p.used());
 
   p.alloc(32);
   CHECK_EQUAL(3, p.nblocks());
-  CHECK_EQUAL(208, p.allocd());
-  CHECK_EQUAL(176, p.used());
+  //CHECK_EQUAL(208, p.allocd());
+  //CHECK_EQUAL(176, p.used());
 
   p.alloc(31);
   CHECK_EQUAL(3, p.nblocks());
-  CHECK_EQUAL(208, p.allocd());
-  CHECK_EQUAL(207, p.used());
+  //CHECK_EQUAL(208, p.allocd());
+  //CHECK_EQUAL(207, p.used());
 
   p.alloc(2);
   CHECK_EQUAL(4, p.nblocks());
-  CHECK_EQUAL(272, p.allocd());
-  CHECK_EQUAL(209, p.used());
+  //CHECK_EQUAL(272, p.allocd());
+  //CHECK_EQUAL(209, p.used());
 }
 
 }  // SUITE
